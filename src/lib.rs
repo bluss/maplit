04479798a62c21af60d8c42d0599632b5cf31804
Alex <blackanger.z@gmail.com>
2018-03-11 20:18:17 +0800

    refactor hashmap!
    Reduce one recursion with `$(,)*`  for hashmap!
    
    ```
    ($($key:expr => $value:expr),* $(,)*) => {
    ```

--- src/lib.rs
+++ src/lib.rs
@@ -47,8 +47,8 @@ macro_rules! hashmap {
     (@single $($x:tt)*) => (());
     (@count $($rest:expr),*) => (<[()]>::len(&[$(hashmap!(@single $rest)),*]));
 
-    ($($key:expr => $value:expr,)+) => { hashmap!($($key => $value),+) };
-    ($($key:expr => $value:expr),*) => {
+//     ($($key:expr => $value:expr,)+) => { hashmap!($($key => $value),+) };
+    ($($key:expr => $value:expr),* $(,)*) => {
         {
             let _cap = hashmap!(@count $($key),*);
             let mut _map = ::std::collections::HashMap::with_capacity(_cap);
