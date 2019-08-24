1efb1c1b0e97f93a7dc7e699b36e15a7c7803457
bluss <bluss@users.noreply.github.com>
2019-08-24 19:01:41 +0200

    FIX: Fix usage of self referential macros for Rust 2018 edition
    For this edition we need #[macro_export(local_inner_macros)] which
    is backwards compatible because it is ignored on older versions.
    
    This enables the self-referential macros.

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@
 //! Generic container macros already exist elsewhere, so those are not provided
 //! here at the moment.
 
-#[macro_export]
+#[macro_export(local_inner_macros)]
 /// Create a **HashMap** from a list of key-value pairs
 ///
 /// ## Example
@@ -74,7 +74,7 @@ macro_rules! hashmap {
 /// assert!(!set.contains("c"));
 /// # }
 /// ```
-#[macro_export]
+#[macro_export(local_inner_macros)]
 macro_rules! hashset {
     (@single $($x:tt)*) => (());
     (@count $($rest:expr),*) => (<[()]>::len(&[$(hashset!(@single $rest)),*]));
@@ -92,7 +92,7 @@ macro_rules! hashset {
     };
 }
 
-#[macro_export]
+#[macro_export(local_inner_macros)]
 /// Create a **BTreeMap** from a list of key-value pairs
 ///
 /// ## Example
@@ -125,7 +125,7 @@ macro_rules! btreemap {
     };
 }
 
-#[macro_export]
+#[macro_export(local_inner_macros)]
 /// Create a **BTreeSet** from a list of elements.
 ///
 /// ## Example
@@ -214,7 +214,7 @@ pub fn __id<T>(t: T) -> T { t }
 ///
 /// # }
 /// ```
-#[macro_export]
+#[macro_export(local_inner_macros)]
 macro_rules! convert_args {
     (keys=$kf:expr, $macro_name:ident !($($k:expr),* $(,)*)) => {
         $macro_name! { $(($kf)($k)),* }
